39ea5e1e4d686746d492f9fca96d4f6253f6c568
William Lee <wjl57@cornell.edu>
2016-06-20 02:10:12 -0400

    Base Kotlin project

---
+++ src/main/kotlin/drivers/helloWorld.kt
@@ -0,0 +1,17 @@
+package drivers
+
+/**
+ * Created by william on 6/20/16.
+ */
+
+fun getGreeting(): String {
+    val words = mutableListOf<String>()
+    words.add("Hello,")
+    words.add("world!")
+
+    return words.joinToString(separator = " ")
+}
+
+fun main(args: Array<String>) {
+    println(getGreeting())
+}
