45b032322246795498756666c611de0ad018bf23
William Lee <wjl57@cornell.edu>
2016-06-30 03:01:18 -0400

    CORS stuff works, but it's hacky right now

--- src/main/kotlin/server/LinFamilyServer.kt
+++ src/main/kotlin/server/LinFamilyServer.kt
@@ -1,6 +1,11 @@
 package server
 
 import org.wasabi.app.AppServer
+import org.wasabi.interceptors.CORSInterceptor
+import org.wasabi.interceptors.enableAutoOptions
+import org.wasabi.interceptors.enableCORS
+import org.wasabi.interceptors.enableCORSGlobally
+import org.wasabi.protocol.http.CORSEntry
 import org.wasabi.routing.routeHandler
 import server.repositories.MySqlDataRepository
 
@@ -11,15 +16,34 @@ class LinFamilyServer {
     private val _dataRepository: MySqlDataRepository = MySqlDataRepository()
 
     fun StartServer(): Unit {
-        var server = AppServer()
-
+        val server = AppServer()
+//        server.enableCORSGlobally()
+//        server.enableAutoOptions()
+//        val corsEntry = CORSEntry (
+//            path = "*",
+//            origins = "*",
+//            methods = "GET, POST, PUT, DELETE, OPTIONS",
+//            headers = "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin",
+//            credentials = ""
+//        )
+//        server.enableCORS(arrayListOf(corsEntry))
+        server.options("/user", sendCors)
         server.get("/", { response.send("Hello World!") })
         server.get("/user", getUser)
         server.start()
     }
 
+    val sendCors = routeHandler {
+        response.addRawHeader("Access-Control-Allow-Origin", "*")
+        response.addRawHeader("Access-Control-Allow-Credentials", "true");
+        response.addRawHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+        response.addRawHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        response.send("")
+    }
+
     val getUser = routeHandler {
         val userId = request.queryParams["userId"]?.toInt() ?: throw Exception("UserId cannot be null")
-        response.send(_dataRepository.GetUser(userId))
+        response.addRawHeader("Access-Control-Allow-Origin", "*")
+        response.send(_dataRepository.GetUser(userId), "application/json")
     }
 }
